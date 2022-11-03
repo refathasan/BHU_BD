dab42cbca86e5e0f75b3a601eab58b290c34e50b
refathasan <refat.aiub@gmail.com>
2022-11-04 02:30:49 +0530

    first commit

---
+++ app/src/main/java/com/refathasan/bhu_bd/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.refathasan.bhu_bd
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+        }, 3000)
+    }
+}
