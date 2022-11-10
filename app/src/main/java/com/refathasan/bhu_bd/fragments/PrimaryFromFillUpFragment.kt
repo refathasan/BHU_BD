1126ee56e191d0622b8f7df8bb055c062a3ae21c
refat hasan <refat.aiub@hotmail.com>
2022-11-11 03:51:20 +0600

    Commit - 8
    Add PDFViewer in Fragment

--- app/src/main/java/com/refathasan/bhu_bd/fragments/PrimaryFromFillUpFragment.kt
+++ app/src/main/java/com/refathasan/bhu_bd/fragments/PrimaryFromFillUpFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.pdfview.PDFView
 import com.refathasan.bhu_bd.R
 
 
 class PrimaryFromFillUpFragment : Fragment() {
+    lateinit var pdfViewer:PDFView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_primary_from_fillup, container, false)
+        var view:View = inflater.inflate(R.layout.fragment_primary_from_fillup,container,false)
+        //return inflater.inflate(R.layout.fragment_primary_from_fillup, container, false)
+        pdfViewer = view.findViewById(R.id.PrimaryFromFillUpPDF)
+        pdfViewer.fromAsset("mathematics.pdf")
+       // pdfViewer.getPageCount()
+        pdfViewer.show()
+        return view
     }
 }
