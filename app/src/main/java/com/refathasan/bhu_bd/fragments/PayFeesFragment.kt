44c3ea38784d792570463e0cef237376add463cb
refat hasan <refat.aiub@hotmail.com>
2022-11-25 12:55:57 +0600

    Commit - 9
    Add PDFViewer in Fragment

--- app/src/main/java/com/refathasan/bhu_bd/fragments/PayFeesFragment.kt
+++ app/src/main/java/com/refathasan/bhu_bd/fragments/PayFeesFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.barteksc.pdfviewer.PDFView
 import com.refathasan.bhu_bd.R
 
 
 class PayFeesFragment : Fragment() {
+    lateinit var pdfView: PDFView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pay_fees, container, false)
+        var view: View = inflater.inflate(R.layout.fragment_primary_from_fillup, container, false)
+        //return inflater.inflate(R.layout.fragment_primary_from_fillup, container, false)
+        pdfView = view.findViewById(R.id.pdfView)
+        pdfView.fromAsset("pdf_test.pdf")
+            .enableSwipe(true)
+            .swipeHorizontal(false)
+            .enableDoubletap(true)
+            .defaultPage(0)
+
+            .load()
+
+        return view
     }
 
 }
